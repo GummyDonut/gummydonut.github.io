d4aec929383014c86d89941a49bb3100722af8b1
lantern77 <peter_hoang2@hotmail.com>
2018-04-04 19:13:15 -0400

    Added scss loading using vue-loader and Loading images

--- src/components/partials/BlogSnippet.vue
+++ src/components/partials/BlogSnippet.vue
@@ -11,26 +11,37 @@ export default{
 <template>
     <div class="pure-g blog-snippet">
        <div class="pure-u-1 pure-u-md-1-6">
-         TEST
+           <img :src="'/static/' + jsonData.thumbnail">
        </div>
        <div class="pure-u-1 pure-u-md-5-6">
            <h1>{{jsonData.title}}</h1>
            <p>
-             {{jsonData.snippet}} 
+             {{jsonData.snippet}}
            </p>
        </div>
        <hr>
     </div>
 </template>
-<style>
+<style lang="scss">
     div.blog-snippet {
         color:white;
-    }
-
-    .blog-snippet h1 {
-        font-family: 'IBM Plex Sans', 'Helvetica Neue', Arial, sans-serif;
-        font-weight:400;
-        font-style:normal;
-    }
-
+        margin-top:1em;
+        h1 {
+            font-family: 'IBM Plex Sans', 'Helvetica Neue', Arial, sans-serif;
+            font-weight:400;
+            font-style:normal;
+        }
+        p {
+            font-family: 'IBM Plex Serif', 'Georgia', Times, serif;
+        }
+        hr {
+            height:0.1px;
+            width:80%;
+            margin-left:0;
+        }
+        img {
+            width:100%;
+            height:100px;
+        }
+   }
 </style>
