22f5fe37340b04c15f76c548d1073647e38222cd
lantern77 <peter_hoang2@hotmail.com>
2018-07-11 18:30:33 -0400

    New particles for resume page, need to work out on how to get right values so not laagy

--- src/components/pages/resume/Resume.vue
+++ src/components/pages/resume/Resume.vue
@@ -1,18 +1,22 @@
-<template>
-  <div class="resume">
-    Resume section
-  </div>
-</template>
-
 <script>
+import particlesJs from '@/components/partials/ParticlesJS'
 export default {
   name: 'Resume',
   data () {
     return {
     }
+  },
+  components: {
+    particlesJs
   }
 }
 </script>
+<template>
+    <div>
+      <particles-js />
+      <h1>TESTOUTSIDE</h1>
+    </div>
+</template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
