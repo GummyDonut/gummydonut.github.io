70cae61c3ad16fdda5184476860a3dcb0ea09112
lantern77 <peter_hoang2@hotmail.com>
2018-08-05 13:54:39 -0400

    Skeleton for about page

--- src/components/pages/about/About.vue
+++ src/components/pages/about/About.vue
@@ -1,6 +1,53 @@
 <template>
-  <div class="about">
-    <h1>Gamers Don't Die They Respawn</h1>
+  <div class="center-align about">
+    <br>
+    <br>
+    <div style="width:auto;display:inline-block;" class="about-section element-shadow">
+      <h1 style="margin-top:0em;">Insert Quote here</h1>
+      <!-- Replace with profile pic here -->
+      <img src="static/img/logo.png" class="profile-img">
+      <br>
+      <p style="text-align:center; margin:0">
+        Peter Hoang
+      </p>
+      <hr style="width:45%;">
+      <small>
+       December 07 1994
+      </small>
+      <br>
+      <small>
+      Software Developer and Hobbyist
+      </small>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="pure-g about-section element-shadow">
+      <div class="pure-u-1 pure-u-md-1-4">
+      </div>
+      <div class="pure-u-1 pure-u-md-3-4" >
+        <h1>Insert title here</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+      </div>
+    </div>
+    <br>
+    <br>
+    <div class="pure-g about-section element-shadow">
+      <div class="pure-u-1 pure-u-md-3-4" >
+        <h1>Insert title here</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+      </div>
+      <div class="pure-u-1 pure-u-md-1-4">
+      </div>
+    </div>
+    <br>
+    <br>
+    <!--List companies that I worked with -->
+    <!--Show social media links -->
+    <div class="pure-g about-section element-shadow">
+      <div class="pure-u-1">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,5 +62,33 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+
+
+.about {
+  color:white;
+}
+.about-section {
+  background-color:#3d4446;
+  padding:1em;
+  margin-left:1em;
+  margin-right:1em;
+  p {
+    text-align:left;
+    font-size:16px;
+  }
+  h1 {
+    text-align:left;
+  }
+}
+.center-align {
+  text-align:center;
+  margin-left:auto;
+  margin-right:auto;
+}
+
+.profile-img {
+  width:164px;
+  height:164px;
+}
 </style>
