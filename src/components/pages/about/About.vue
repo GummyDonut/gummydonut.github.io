f557cf36ddb3f492dbf1b1245cb24c8505a7b703
lantern77 <peter_hoang2@hotmail.com>
2018-09-01 10:35:42 -0400

    Update about, its been awhile

--- src/components/pages/about/About.vue
+++ src/components/pages/about/About.vue
@@ -25,7 +25,7 @@
     <div class="pure-g about-section element-shadow">
       <div class="pure-u-1 pure-u-md-1-4">
       </div>
-      <div class="pure-u-1 pure-u-md-3-4" >
+      <div style="text-align:right;" class="pure-u-1 pure-u-md-3-4" >
         <h1>Insert title here</h1>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
       </div>
@@ -33,7 +33,7 @@
     <br>
     <br>
     <div class="pure-g about-section element-shadow">
-      <div class="pure-u-1 pure-u-md-3-4" >
+      <div style="text-align:left;" class="pure-u-1 pure-u-md-3-4" >
         <h1>Insert title here</h1>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
       </div>
@@ -44,8 +44,32 @@
     <br>
     <!--List companies that I worked with -->
     <!--Show social media links -->
+
     <div class="pure-g about-section element-shadow">
       <div class="pure-u-1">
+        <h1>Hobbies</h1>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+        </p>
+      </div>
+    </div>
+    <br>
+    <br>
+    <div class="pure-g">
+      <div class="pure-u-1 pure-u-md-1-3 hobby-section">
+        <div class="element-shadow">
+          Test
+        </div>
+      </div>
+      <div class="pure-u-1 pure-u-md-1-3 hobby-section">
+        <div class="element-shadow">
+          Test
+        </div>
+      </div>
+      <div class="pure-u-1 pure-u-md-1-3 hobby-section">
+        <div class="element-shadow">
+          Test
+        </div>
       </div>
     </div>
   </div>
@@ -63,10 +87,14 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-
-
 .about {
   color:white;
+}
+.hobby-section {
+  padding:1em;
+  div {
+    background-color:#3d4446;
+  }
 }
 .about-section {
   background-color:#3d4446;
@@ -74,11 +102,7 @@ export default {
   margin-left:1em;
   margin-right:1em;
   p {
-    text-align:left;
     font-size:16px;
-  }
-  h1 {
-    text-align:left;
   }
 }
 .center-align {
