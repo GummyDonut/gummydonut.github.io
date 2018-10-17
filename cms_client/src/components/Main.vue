d9b0d960a8022bf702dab7271511b8775db952d5
lantern77 <peter_hoang2@hotmail.com>
2018-10-17 12:27:54 -0400

    basic pure css for client side

--- cms_client/src/components/Main.vue
+++ cms_client/src/components/Main.vue
@@ -9,94 +9,21 @@ export default {
   },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
 </script>
 
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="main">
+    <div class="pure-menu pure-menu-horizontal">
+      <a href="#" class="pure-menu-heading pure-menu-link">BRAND</a>
+      <ul class="pure-menu-list">
+          <li class="pure-menu-item"><a href="#" class="pure-menu-link">News</a></li>
+          <li class="pure-menu-item"><a href="#" class="pure-menu-link">Sports</a></li>
+          <li class="pure-menu-item"><a href="#" class="pure-menu-link">Finance</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 
--- cms_client/src/main.js
+++ cms_client/src/main.js
@@ -4,16 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
-import '../static/css/bootstrap.min.css'
-import * as uiv from 'uiv'
 
 Vue.config.productionTip = false
 let devAxios = axios.create({
   'baseURL': 'http://localhost:3000/'
 })
-Vue.prototype.$axios = devAxios
 
-Vue.use(uiv)
+Vue.prototype.$axios = devAxios
 
 /* eslint-disable no-new */
 new Vue({
